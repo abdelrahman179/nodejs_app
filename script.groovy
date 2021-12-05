b40dfab7c4d3a67627afb65fe22c17d337fd27e7
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2021-12-05 04:22:23 +0200

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def CI() {
+    echo "building the docker image..."
+     withCredentials([usernamePassword(credentialsId: 'Docker_Hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                    sh 'docker build . -f dockerfile -t abdelrahmanzaki179/node-js-app:latest'
+                    sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+                    sh 'docker push abdelrahmanzaki179/node-js-app:latest'
+                }
+} 
+
+def CD() {
+    echo 'Running the application on localhost:3000 successfully ... '
+    withCredentials([usernamePassword(credentialsId: 'Docker_Hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                  sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+                  sh 'docker run -p 3000:3000 -d abdelrahmanzaki179/node-js-app:latest'
+              }
+} 
+
+return this
